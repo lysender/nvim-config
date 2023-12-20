fa26fc1b4e7f4e5af76fc35dc0d7e1614cb6e4a6
Lysender <developer@lysender.com>
2023-12-20 12:42:01 +0000

    fix: update base vim config

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,5 +1,13 @@
-vim.wo.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-vim.o.colorcolumn = '120'
-vim.o.autoindent = true
-vim.o.smartindent = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '120'
