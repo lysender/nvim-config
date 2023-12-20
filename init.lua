fb9b3413512889e92dcf61fdaf353680a11f679e
Lysender <developer@lysender.com>
2023-12-20 13:28:11 +0000

    fix: path to custom remap

--- init.lua
+++ init.lua
@@ -677,7 +677,7 @@ cmp.setup {
 }
 
 -- Custom keybinding remaps
-require('lua.custom.remap')
+require('custom.remap')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
