4af075070ef24248acbdbd1f69061c06962cce36
Lysender <lysender@darkstar-win>
2023-12-22 12:53:08 +0800

    fix: set default tab size

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,6 +1,12 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Default tab settings
+-- Override tab settings by using .editorconfig
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
