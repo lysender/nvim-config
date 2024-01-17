12803cf1057ce4d946f9a9faee4e6bc0bf430d89
Lysender <lysender@darkstar-win>
2024-01-17 09:53:46 +0800

    fix: add remap for better paste flow

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 -- Move highlighted block up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Paste highlighted text without losing the previous text from clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
