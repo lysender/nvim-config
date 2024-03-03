2f74fdd016a25ae5aa4995e0fba64f8114aff4f3
Lysender <lysender@darkstar-win>
2024-03-04 07:52:24 +0800

    fix: ensure preferred lsp are installed, enable some formatters

--- init.lua
+++ init.lua
@@ -588,7 +588,12 @@ require("lazy").setup({
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
+				"css-lsp",
+				"html-lsp",
+				"htmx-lsp",
+				"rust-analyzer",
 				"stylua", -- Used to format lua code
+				"typescript-language-server",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -622,7 +627,9 @@ require("lazy").setup({
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				rust = { "rustfmt" },
 			},
 		},
 	},
