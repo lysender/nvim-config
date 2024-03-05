ba0be40d1551f97e69ad1899bb2df1dadef26f1a
Lysender <lysender@darkstar-win>
2024-03-05 15:19:50 +0800

    fix: conform.nvim should not use lsp formatting

--- init.lua
+++ init.lua
@@ -618,7 +618,7 @@ require("lazy").setup({
 			notify_on_error = false,
 			format_on_save = {
 				timeout_ms = 500,
-				lsp_fallback = true,
+				lsp_fallback = false,
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
