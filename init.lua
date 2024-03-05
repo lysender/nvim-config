13daef594ea879d60403a085cb69d504bd88b071
Lysender <lysender@darkstar-win>
2024-03-05 12:09:44 +0800

    fix: disable js prettier, not worth it

--- init.lua
+++ init.lua
@@ -618,7 +618,7 @@ require("lazy").setup({
 			notify_on_error = false,
 			format_on_save = {
 				timeout_ms = 500,
-				lsp_fallback = false,
+				lsp_fallback = true,
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
@@ -627,8 +627,7 @@ require("lazy").setup({
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
+				-- javascript = { { "prettierd", "prettier" } },
 				rust = { "rustfmt" },
 			},
 		},
