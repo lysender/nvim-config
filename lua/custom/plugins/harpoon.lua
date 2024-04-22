ab3deb511e81d96540c4eecbea84951ff4b1e2fe
Lysender <lysender@darkstar-win>
2024-04-22 16:10:43 +0800

    fix: change harpoon mapping

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,20 +1,31 @@
 return {
-  "ThePrimeagen/harpoon",
-  name = "harpoon",
-  branch = "harpoon2",
-  requires = { {"nvim-lua/plenary.nvim"} },
-  config = function ()
-    local harpoon = require("harpoon")
+	"ThePrimeagen/harpoon",
+	name = "harpoon",
+	branch = "harpoon2",
+	requires = { { "nvim-lua/plenary.nvim" } },
+	config = function()
+		local harpoon = require("harpoon")
 
-    harpoon:setup()
+		harpoon:setup()
 
+		vim.keymap.set("n", "<C-p>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-p>l", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-    vim.keymap.set("n", "<leader>as", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-    --vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    --vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    --vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    --vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-  end
+		vim.keymap.set("n", "<C-p>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-p>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-p>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-p>4", function()
+			harpoon:list():select(4)
+		end)
+	end,
 }
