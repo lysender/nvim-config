3e0c757f961fbbf16abf0e7c7a7f630c667d604d
Lysender <lysender@darkstar-win>
2025-02-05 14:15:59 +0800

    fix: remove unused lsp

--- init.lua
+++ init.lua
@@ -652,9 +652,7 @@ require("lazy").setup({
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"css-lsp",
 				"gopls",
-				"html-lsp",
 				"rust-analyzer",
 				"stylua", -- Used to format Lua code
 			})
