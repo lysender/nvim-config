ec72d0493427637317571519027a4b82aa681ee0
Lysender <lysender@darkstar-win>
2025-03-27 12:13:21 +0800

    fix: add organize imports

--- init.lua
+++ init.lua
@@ -719,10 +719,10 @@ require("lazy").setup({
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
-				javascript = { "biome" },
-				javascriptreact = { "biome" },
-				typescript = { "biome" },
-				typescriptreact = { "biome" },
+				javascript = { "biome", "biome-organize-imports" },
+				javascriptreact = { "biome", "biome-organize-imports" },
+				typescript = { "biome", "biome-organize-imports" },
+				typescriptreact = { "biome", "biome-organize-imports" },
 				go = { "goimports", "gofmt" },
 				rust = { "rustfmt" },
 			},
