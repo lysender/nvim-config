fa31674084ffbb4225a5addff4a6044ecea8d4a8
Lysender <lysender@darkstar-win>
2026-01-13 14:50:09 +0800

    fix: disable snacks scroll

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,9 +2,8 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      indent = {
-        enabled = false,
-      },
+      indent = { enabled = false },
+      scroll = { enabled = false },
     },
   },
 }
